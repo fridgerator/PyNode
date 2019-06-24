1551efb7e56d6c9be5f6c0fd377e7d6b95911a1f
Nick Franken <shnick@gmail.com>
2019-06-24 12:37:17 -0500

    fixed typings

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
-export function startInterpreter(pythonpath?: string): void;
-export function appendSysPath(path: string): void;
-export function openFile(filename: string): void;
-export function call(...args: any[]): any;
+declare module 'pynode' {
+  export function startInterpreter(pythonpath?: string): void;
+  export function appendSysPath(path: string): void;
+  export function openFile(filename: string): void;
+  export function call(...args: any[]): any;
+}
