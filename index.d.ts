f3d563f449736da061794c9dc9551a40b216ab36
Nick Franken <shnick@gmail.com>
2019-06-24 12:15:37 -0500

    fixed typings

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export function startInterpreter(pythonpath?: string): void;
+export function appendSysPath(path: string): void;
+export function openFile(filename: string): void;
+export function call(...args: any[]): any;
--- pynode.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace pynode {
-  declare function startInterpreter(pythonpath?: string): void;
-  declare function appendSysPath(path: string): void;
-  declare function openFile(filename: string): void;
-  declare function call(...args: any[]): any;
-}
