4f0134bdea82772189de2f63bda720c26d43a0f9
Nick Franken <shnick@gmail.com>
2019-10-11 10:22:52 -0500

    trying to get multiprocessing to work

--- test_files/performance.py
+++ test_files/performance.py
@@ -10,19 +10,21 @@
 from math import pow, atan, tan
 import multiprocessing
 
+queue = multiprocessing.Queue()
+
 def job(queue, num1, num2):
   base = num1 + num2
   result = 0
   for i in range(int(pow(base, 7))):
     result += atan(i) * tan(i)
   print("result : ", result, flush=True)
-  queue.put(result)
+  queue.put_nowait(result)
 
 def generate_slow_number(num1, num2):
-  queue = multiprocessing.Queue()
+  global queue
   p = multiprocessing.Process(target=job, args=(queue, num1, num2, ))
   p.start()
-  x = queue.get()
+  x = queue.get(block=True)
   p.join()
   print("return x : ", x, flush=True)
   return x
