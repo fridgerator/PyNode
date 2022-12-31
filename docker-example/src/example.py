23f4a6336408b1cf938bea4722bcfc905f4a1471
ozan <ozanerturk17@gmail.com>
2022-12-31 21:39:28 +0300

    docker example

---
+++ docker-example/src/example.py
@@ -0,0 +1,10 @@
+import scipy
+import os
+def test():
+    #print python version
+    print("python version:",os.sys.version)
+    #print current working directory
+    print("cwd:",os.getcwd())
+    #print environment variables
+    print(scipy.rand())
+    return True
